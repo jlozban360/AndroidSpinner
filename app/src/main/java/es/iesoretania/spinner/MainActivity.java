370de122a4377f546906a207c3a74bd9da0fc540
jlozban360 <jlozban360@g.educaand.es>
2024-10-14 12:42:58 +0200

    Final release

--- app/src/main/java/es/iesoretania/spinner/MainActivity.java
+++ app/src/main/java/es/iesoretania/spinner/MainActivity.java
@@ -1,6 +1,12 @@
 package es.iesoretania.spinner;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,5 +32,43 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater( ) );
         setContentView( binding.getRoot( ) );
+
+        Spinner spinner = findViewById( R.id.spinner );
+
+        String[ ] operaciones = { "Suma", "Resta", "Multiplicación", "División" };
+        ArrayAdapter <String> adapter = new ArrayAdapter<>( this, R.layout.spinner_item, operaciones );
+        adapter.setDropDownViewResource(R.layout.spinner_item);
+        spinner.setAdapter( adapter );
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String opcionSeleccionada = (String) spinner.getItemAtPosition( i );
+
+                if( binding.editTextNumber1.getText( ).toString( ).isEmpty( ) || binding.editTextNumber2.getText( ).toString( ).isEmpty( ) ) {
+                    return;
+                }
+
+                int valor1 = Integer.parseInt( binding.editTextNumber1.getText( ).toString( ) );
+                int valor2 = Integer.parseInt( binding.editTextNumber2.getText( ).toString( ) );
+                int resultado = 0;
+
+                switch( opcionSeleccionada )
+                {
+                    case "Suma": resultado = valor1 + valor2; break;
+                    case "Resta": resultado = valor1 - valor2; break;
+                    case "Multiplicación": resultado = valor1 * valor2; break;
+                    case "División": resultado = valor1 / valor2; break;
+                }
+
+                binding.textViewResultado.setText( String.valueOf( resultado ) );
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
     }
+
+
 }
